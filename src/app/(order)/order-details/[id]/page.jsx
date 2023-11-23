d58ab09ee0f4ba904e4958f127f5e4001a7fc266
so <ansso8430@gmail.com>
2023-11-23 16:50:41 +0900

    file settings

---
+++ src/app/(order)/order-details/[id]/page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const OrderDetails = ({ params, searchParams }) => {
+  const { id } = params;
+  const { hello } = searchParams;
+  return (
+    <div>
+      {id} <br />
+      {hello}
+    </div>
+  );
+};
+
+export default OrderDetails;
---
+++ src/components/iconInput/IconInput.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const IconInput = () => {
+  return <div></div>;
+};
+
+export default IconInput;
