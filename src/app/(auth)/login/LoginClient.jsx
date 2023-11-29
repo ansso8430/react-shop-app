d131c9bcfe0facc9dffd0c456384e6020ddd08a6
so <ansso8430@gmail.com>
2023-11-29 18:24:28 +0900

    login page

--- src/app/(auth)/login/LoginClient.jsx
+++ src/app/(auth)/login/LoginClient.jsx
@@ -7,6 +7,10 @@ import LogoPath from "@/assets/colorful.svg";
 import Loader from "@/components/loader/Loader";
 import Input from "@/components/Input/Input";
 import styles from "./Auth.module.scss";
+import AutoSignInCheckbox from "@/components/autoSingInCheckbox/AutoSignInCheckbox";
+import Divider from "@/components/divider/Divider";
+import Button from "@/components/button/Button";
+import Link from "next/link";
 
 const LoginClient = () => {
   const [email, setEmail] = useState("");
@@ -62,13 +66,41 @@ const LoginClient = () => {
             />
 
             <div className={styles.group}>
-              {/* 자동 로그인, 비밀번호 수정 */}
-              자동 로그인, 비밀번호 수정
+              <AutoSignInCheckbox
+                checked={isAutoLogin}
+                onChange={(e) => setIsAutoLogin(e.target.checked)}
+              />
+
+              <Link href={"/reset"} className={styles.findLink}>
+                비밀번호 수정하기
+                <svg
+                  width="11"
+                  height="18"
+                  viewBox="0 0 11 18"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                  className={styles.findLinkArrow}
+                >
+                  <path
+                    d="M1.5 1L9.5 9L1.5 17"
+                    stroke="#0074E9"
+                    strokeWidth="2"
+                  />
+                </svg>
+              </Link>
             </div>
             <div className={styles.buttonGroup}>
-              {/* button */}
-              button
-              <div>{/* button */}</div>
+              <Button type="submit" width="100%">
+                로그인
+              </Button>
+              <Divider />
+              <Button width="100%" secondary>
+                <Link href={"/register"}>회원가입</Link>
+              </Button>
+              <Divider />
+              <div>
+                <Button onClick={signInWithGoogle}>구글 로그인</Button>
+              </div>
             </div>
           </form>
         </div>
